7a5818cb0b8d2dff84dd5bb9924dfd2c45530445
David Hoppenbrouwers <appender@demindiro.com>
2025-12-14 16:29:37 +0100

    device: remove unused optimal_alignment()

--- src/device.rs
+++ src/device.rs
@@ -14,7 +14,6 @@ pub trait Device {
     fn write<'a>(&'a self, bytes: usize) -> Result<Self::Write<'a>, Self::Error>;
 
     fn len(&self) -> u64;
-    fn optimal_alignment(&self) -> Alignment;
 
     fn sync(&self) -> Result<(), Self::Error>;
     fn wipe(&self) -> Result<(), Self::Error>;
@@ -26,42 +25,6 @@ pub trait Write {
     fn append(&mut self, data: &[u8]) -> Result<(), Self::Error>;
 
     fn offset(&self) -> u64;
-}
-
-pub enum Alignment {
-    N0 = 0,
-    N1 = 1,
-    N2 = 2,
-    N3 = 3,
-    N4 = 4,
-    N5 = 5,
-    N6 = 6,
-    N7 = 7,
-    N8 = 8,
-    N9 = 9,
-    N10 = 10,
-    N11 = 11,
-    N12 = 12,
-    N13 = 13,
-    N14 = 14,
-    N15 = 15,
-    N16 = 16,
-    N17 = 17,
-    N18 = 18,
-    N19 = 19,
-    N20 = 20,
-    N21 = 21,
-    N22 = 22,
-    N23 = 23,
-    N24 = 24,
-    N25 = 25,
-    N26 = 26,
-    N27 = 27,
-    N28 = 28,
-    N29 = 29,
-    N30 = 30,
-    N31 = 31,
-    N32 = 32,
 }
 
 pub struct VecWrite<'a, T> {
@@ -108,10 +71,6 @@ impl Device for RefCell<Vec<u8>> {
 
     fn len(&self) -> u64 {
         self.borrow().len().try_into().unwrap_or(u64::MAX)
-    }
-
-    fn optimal_alignment(&self) -> Alignment {
-        Alignment::N0
     }
 
     fn wipe(&self) -> Result<(), Self::Error> {
