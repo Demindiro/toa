97dd6035b38757ab91e339def45a6b46ac4f2fb8
David Hoppenbrouwers <appender@demindiro.com>
2025-12-17 00:24:58 +0100

    manifest.scm: add clang-toolchain for zstd_safe

--- manifest.scm
+++ manifest.scm
@@ -3,4 +3,4 @@
 ;; that accepts a '--manifest' (or '-m') option.
 
 (specifications->manifest
-  (list "rust" "rust:cargo" "rust:tools"))
+  (list "rust" "rust:cargo" "rust:tools" "clang-toolchain"))
