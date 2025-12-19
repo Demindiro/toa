8b8f992d4200a9cba634e118c62d6bd0eb0983af
David Hoppenbrouwers <appender@demindiro.com>
2025-12-19 02:27:05 +0100

    appender: unconditionally advance Builder::pack_len in flush_leaf()
    A subtle logic bug, but luckily spotted before it could have become an
    issue.

--- appender/src/builder.rs
+++ appender/src/builder.rs
@@ -123,10 +123,10 @@ where
     fn flush_leaf(&mut self) -> Result<(), Error<D::Error>> {
         if let Some(x) = self.flush_record(0)? {
             self.append_record_parent(x)?;
-            const MASK: u64 = (1 << PITCH) - 1;
-            self.pack_len.0 += MASK;
-            self.pack_len.0 &= !MASK;
         }
+        const MASK: u64 = (1 << PITCH) - 1;
+        self.pack_len.0 += MASK;
+        self.pack_len.0 &= !MASK;
         Ok(())
     }
 
