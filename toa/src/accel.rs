14380add517ec98157c8f81cf4f13d6482c3a552
David Hoppenbrouwers <appender@demindiro.com>
2026-04-12 08:28:05 +0200

    toa: remove accel::HashCache

--- toa/src/accel.rs
+++ toa/src/accel.rs
@@ -1,10 +1,7 @@
 #[cfg(feature = "accel-sled")]
 pub use sled;
 
-use std::{
-    collections::BTreeMap,
-    io::{self, Read, Seek, Write},
-};
+use std::{collections::BTreeMap, io};
 use toa_hash::Hash;
 
 /// erated index.
@@ -39,22 +36,6 @@ pub struct IndexCookie {
 #[repr(transparent)]
 pub struct IndexEntry(pub u64);
 
-/// A very dumb on-disk cache.
-///
-/// Purely intended to be loaded fast into memory.
-pub struct HashCache<T> {
-    file: T,
-    cookie: IndexCookie,
-    map: BTreeMap<Hash, IndexEntry>,
-}
-
-#[derive(Clone, Copy, Default, bytemuck::Zeroable, bytemuck::Pod)]
-#[repr(C)]
-struct HashCacheEntry {
-    key: Hash,
-    value: IndexEntry,
-}
-
 impl Index for BTreeMap<Hash, IndexEntry> {
     fn top_cookie(&self) -> IndexCookie {
         IndexCookie::default()
@@ -74,68 +55,6 @@ impl Index for BTreeMap<Hash, IndexEntry> {
     }
 }
 
-impl<T> HashCache<T>
-where
-    T: Read + Write + Seek,
-{
-    pub fn load(file: T) -> io::Result<Self> {
-        let mut s = Self {
-            file,
-            cookie: Default::default(),
-            map: Default::default(),
-        };
-        s.file.rewind()?;
-        match s.file.read_exact(bytemuck::bytes_of_mut(&mut s.cookie)) {
-            Ok(()) => {}
-            Err(e) if e.kind() == io::ErrorKind::UnexpectedEof => s
-                .file
-                .write_all(bytemuck::bytes_of(&IndexCookie::default()))?,
-            Err(e) => return Err(e),
-        }
-        let mut reader = io::BufReader::with_capacity(1 << 17, &mut s.file);
-        let mut entry = HashCacheEntry::default();
-        while reader
-            .read_exact(bytemuck::bytes_of_mut(&mut entry))
-            .is_ok()
-        {
-            s.map.insert(entry.key, entry.value);
-        }
-        Ok(s)
-    }
-}
-
-impl<T> Index for HashCache<T>
-where
-    T: Read + Write + Seek,
-{
-    fn top_cookie(&self) -> IndexCookie {
-        self.cookie
-    }
-
-    fn get(&self, key: &Hash) -> io::Result<Option<IndexEntry>> {
-        Ok(self.map.get(key).copied())
-    }
-
-    fn add(&mut self, &key: &Hash, value: IndexEntry) -> io::Result<()> {
-        let pos = self.file.seek(io::SeekFrom::End(0))?;
-        assert!(
-            pos >= core::mem::size_of::<IndexCookie>() as u64,
-            "file missing header"
-        );
-        self.map.insert(key, value);
-        let entry = HashCacheEntry { key, value };
-        self.file.write_all(bytemuck::bytes_of(&entry))?;
-        Ok(())
-    }
-
-    fn set_top(&mut self, new_top: IndexCookie) -> io::Result<()> {
-        self.file.seek(io::SeekFrom::Start(0))?;
-        self.file.write_all(bytemuck::bytes_of(&new_top))?;
-        self.file.flush()?;
-        Ok(())
-    }
-}
-
 #[cfg(feature = "accel-sled")]
 mod imp_sled {
     use super::*;
@@ -146,9 +65,7 @@ mod imp_sled {
         fn top_cookie(&self) -> IndexCookie {
             sled::Tree::get(self, KEY_TOP_COOKIE)
                 .unwrap()
-                .map_or_else(Default::default, |x| {
-                    bytemuck::pod_read_unaligned(&x)
-                })
+                .map_or_else(Default::default, |x| bytemuck::pod_read_unaligned(&x))
         }
 
         fn get(&self, key: &Hash) -> io::Result<Option<IndexEntry>> {
@@ -168,41 +85,5 @@ mod imp_sled {
                 .unwrap();
             Ok(())
         }
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-    use crate::*;
-
-    fn init<A>(accel: A) -> crate::test::Test<A>
-    where
-        A: Index,
-    {
-        let store = toa_blob::BlobStore::init(toa_blob::MemZones::new(1 << 20, 20)).unwrap();
-        let toa = Toa::init(store, accel, PageSize::K4, Compression::None, 0)
-            .expect("toa init failed")
-            .expect("duplicate toa store");
-        crate::test::Test { toa }
-    }
-
-    #[test]
-    fn hash_cache() {
-        let accel = std::io::Cursor::new(vec![]);
-        let accel = HashCache::load(accel).unwrap();
-        let mut s = init(accel);
-        let f = |x| format!("A number {x}").into_bytes();
-        let keys = (0..1 << 12).map(|i| s.add(&f(i))).collect::<Vec<_>>();
-        keys.iter()
-            .enumerate()
-            .for_each(|(i, k)| s.assert_eq(k, &f(i)));
-        let (store, accel, res) = s.toa.unmount();
-        res.unwrap();
-        let toa = Toa::load(store, accel).unwrap().unwrap();
-        s = crate::test::Test { toa };
-        keys.iter()
-            .enumerate()
-            .for_each(|(i, k)| s.assert_eq(k, &f(i)));
     }
 }
