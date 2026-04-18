59d762dfd49ad2722c55e82fd5c3bafe8c5b7abc
David Hoppenbrouwers <appender@demindiro.com>
2026-04-18 13:38:03 +0200

    waddle: add test.sh

--- waddle/build.sh
+++ waddle/build.sh
@@ -1,19 +1,2 @@
 #!/bin/sh
-
-set -xe
-
-cd "`dirname "$0"`"
-
-test -d work || ./init.sh
-
-mkdir -p work/target
-
-exec env -i HOME=/root TERM="$TERM" PATH=/bin:/usr/bin:/root/.cargo/bin ./work/bin/waddle \
-	--base work/alpine \
-	--net \
-	--mount-proc \
-	--bind /dev /dev \
-	--bind /rust .. \
-	--bind /target work/target \
-	--cwd /rust \
-	-- /root/.cargo/bin/cargo b --release
+exec `dirname $0`/run.sh /root/.cargo/bin/cargo b --release
